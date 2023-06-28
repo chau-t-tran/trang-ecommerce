a554f21d8a346ae38cd091eaa7aff799638437b4
Chau Tran <chau.t.tran.us@gmail.com>
2023-06-27 22:47:28 -0400

    Added navbar

---
+++ components/NavbarDrawer/NavbarDrawer.styles.tsx
@@ -0,0 +1,13 @@
+import { createStyles } from '@mantine/core';
+
+export default createStyles((theme) => ({
+  overlay: {
+    padding: 0
+  },
+  content: {
+    position: 'absolute',
+    left: 0,
+    height: '100%',
+    radius: 0,
+  },
+}));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
+import { AppShell, Header, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { AppShell, Navbar, Header } from '@mantine/core';
+import { NavbarDrawer } from '../components/NavbarDrawer/NavbarDrawer';
 
 export default function HomePage() {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <>
+      <NavbarDrawer opened={opened} close={close}/>
       <AppShell
         padding="md"
-        navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
-        header={<Header height={60} p="xs">{/* Header content */}</Header>}
+        header={
+          <Header height={60} p="xs">
+            <Burger
+              opened={opened}
+              onClick={open}
+              size="sm"
+              mr="xl" 
+            />
+          </Header>
+        }
         styles={(theme) => ({
           main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
         })}
